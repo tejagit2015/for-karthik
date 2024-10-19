57c72a0c417c30ee9241d73d82ac27ec8f08c918
rav <rangebound1909@gmail.com>
2024-10-19 22:01:45 +0530

    Revert "Initial Commit of Order-Service"
    This reverts commit 9c7955a483614ec11964779721396a240154f4c1.

--- src/main/java/com/fsm/microservice/order/dto/OrderRequest.java
+++
@@ -1,8 +0,0 @@
-package com.fsm.microservice.order.dto;
-
-import java.math.BigDecimal;
-
-public record OrderRequest(Long id, String orderNumber,
-                           String skuCode, BigDecimal price,
-                           Integer quantity) {
-}
